8a9b2d08415e830f3017693bec5fc125b224f39d
Lera Tikhonova <valeria.tihonova2011@yandex.ru>
2026-05-25 23:08:40 +0300

    Исправление: правильное определение ночи для сессии с 22:00 до 8:00

--- src/main/java/ru/yandex/practicum/sleeptracker/analysis/functions/SleeplessNightsCountFunction.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/analysis/functions/SleeplessNightsCountFunction.java
@@ -21,41 +21,34 @@ public class SleeplessNightsCountFunction implements SleepAnalysisFunction<Integ
         Set<LocalDate> allNights = new HashSet<>();
         Set<LocalDate> nightsWithSleep = new HashSet<>();
 
-        System.out.println("=== DEBUG SleeplessNightsCountFunction ===");
-
         for (SleepingSession session : sessions) {
             LocalDateTime start = session.getStartTime();
             LocalDateTime end = session.getEndTime();
-            System.out.println("Session: " + start + " -> " + end);
 
-            LocalDate night1 = start.toLocalDate();
-            if (start.toLocalTime().isBefore(LocalTime.of(6, 0))) {
-                night1 = night1.minusDays(1);
+            LocalDate night = getNightDate(start);
+            LocalDate nextNight = night.plusDays(1);
+
+            allNights.add(night);
+            allNights.add(nextNight);
+
+            if (coversNight(start, end, night)) {
+                nightsWithSleep.add(night);
             }
-            LocalDate night2 = night1.plusDays(1);
-
-            System.out.println("  night1: " + night1);
-            System.out.println("  night2: " + night2);
-
-            allNights.add(night1);
-            allNights.add(night2);
-
-            if (coversNight(start, end, night1)) {
-                nightsWithSleep.add(night1);
-                System.out.println("  + night1 covered");
-            }
-            if (coversNight(start, end, night2)) {
-                nightsWithSleep.add(night2);
-                System.out.println("  + night2 covered");
+            if (coversNight(start, end, nextNight)) {
+                nightsWithSleep.add(nextNight);
             }
         }
 
-        System.out.println("All nights: " + allNights);
-        System.out.println("Nights with sleep: " + nightsWithSleep);
-        System.out.println("Sleepless: " + (allNights.size() - nightsWithSleep.size()));
-
         int sleeplessNights = allNights.size() - nightsWithSleep.size();
         return new SleepAnalysisResult<>(DESCRIPTION, sleeplessNights);
+    }
+
+    private LocalDate getNightDate(LocalDateTime dateTime) {
+        LocalTime time = dateTime.toLocalTime();
+        if (time.isBefore(LocalTime.of(6, 0))) {
+            return dateTime.toLocalDate().minusDays(1);
+        }
+        return dateTime.toLocalDate();
     }
 
     private boolean coversNight(LocalDateTime start, LocalDateTime end, LocalDate night) {
