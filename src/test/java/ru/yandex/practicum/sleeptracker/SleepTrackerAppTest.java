76bc9dd6e7c0a3bbd17ade5d61e0df341c098039
Lera Tikhonova <valeria.tihonova2011@yandex.ru>
2026-05-27 07:34:15 +0300

    Исправление структуры: классы только в main, тесты только в test

--- src/test/java/ru/yandex/practicum/sleeptracker/SleepTrackerAppTest.java
+++ src/test/java/ru/yandex/practicum/sleeptracker/SleepTrackerAppTest.java
@@ -29,10 +29,10 @@ public class SleepTrackerAppTest {
     }
 
     @Test
-    void testSleeplessNightsWithSessionStartingAfterMidnight() {
+    void testSleeplessNightsWithOneSleeplessNight() {
         List<SleepingSession> sessions = Arrays.asList(
-            new SleepingSession(LocalDateTime.of(2024, 1, 2, 1, 0), LocalDateTime.of(2024, 1, 2, 8, 0), SleepQuality.GOOD),
-            new SleepingSession(LocalDateTime.of(2024, 1, 3, 23, 0), LocalDateTime.of(2024, 1, 4, 7, 0), SleepQuality.NORMAL)
+            new SleepingSession(LocalDateTime.of(2024, 1, 1, 22, 0), LocalDateTime.of(2024, 1, 2, 8, 0), SleepQuality.GOOD),
+            new SleepingSession(LocalDateTime.of(2024, 1, 3, 7, 0), LocalDateTime.of(2024, 1, 3, 11, 0), SleepQuality.NORMAL)
         );
         SleeplessNightsCountFunction function = new SleeplessNightsCountFunction();
         assertEquals(1, function.apply(sessions).getValue());
@@ -47,6 +47,26 @@ public class SleepTrackerAppTest {
         );
         SleeplessNightsCountFunction function = new SleeplessNightsCountFunction();
         assertEquals(0, function.apply(sessions).getValue());
+    }
+
+    @Test
+    void testSleeplessNightsWithNoNightSessions() {
+        List<SleepingSession> sessions = Arrays.asList(
+            new SleepingSession(LocalDateTime.of(2024, 1, 1, 7, 0), LocalDateTime.of(2024, 1, 1, 11, 0), SleepQuality.GOOD),
+            new SleepingSession(LocalDateTime.of(2024, 1, 2, 8, 0), LocalDateTime.of(2024, 1, 2, 12, 0), SleepQuality.NORMAL)
+        );
+        SleeplessNightsCountFunction function = new SleeplessNightsCountFunction();
+        assertEquals(2, function.apply(sessions).getValue());
+    }
+
+    @Test
+    void testSleeplessNightsWithSessionStartingAfterMidnight() {
+        List<SleepingSession> sessions = Arrays.asList(
+            new SleepingSession(LocalDateTime.of(2024, 1, 2, 1, 0), LocalDateTime.of(2024, 1, 2, 8, 0), SleepQuality.GOOD),
+            new SleepingSession(LocalDateTime.of(2024, 1, 3, 23, 0), LocalDateTime.of(2024, 1, 4, 7, 0), SleepQuality.NORMAL)
+        );
+        SleeplessNightsCountFunction function = new SleeplessNightsCountFunction();
+        assertEquals(1, function.apply(sessions).getValue());
     }
 
     @Test
