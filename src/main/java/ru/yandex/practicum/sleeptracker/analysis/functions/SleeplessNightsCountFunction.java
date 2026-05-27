5890890e23b8ffa7f123bfa101d63c71e8f377b3
Lera Tikhonova <valeria.tihonova2011@yandex.ru>
2026-05-27 07:01:43 +0300

    Точная копия из успешного PR

--- src/main/java/ru/yandex/practicum/sleeptracker/analysis/functions/SleeplessNightsCountFunction.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/analysis/functions/SleeplessNightsCountFunction.java
@@ -17,6 +17,7 @@ public class SleeplessNightsCountFunction implements SleepAnalysisFunction<Integ
             return new SleepAnalysisResult<>(DESCRIPTION, 0);
         }
 
+        Set<LocalDate> allNights = new HashSet<>();
         Set<LocalDate> nightsWithSleep = new HashSet<>();
 
         for (SleepingSession session : sessions) {
@@ -24,27 +25,22 @@ public class SleeplessNightsCountFunction implements SleepAnalysisFunction<Integ
             LocalDateTime end = session.getEndTime();
 
             LocalDate night = start.toLocalDate();
-            if (start.getHour() < 6) {
-                night = night.minusDays(1);
+            if (start.getHour() >= 12) {
+                night = night.plusDays(1);
             }
+
+            allNights.add(night);
+            allNights.add(night.minusDays(1));
 
             if (coversNight(start, end, night)) {
                 nightsWithSleep.add(night);
-            } else if (coversNight(start, end, night.plusDays(1))) {
-                nightsWithSleep.add(night.plusDays(1));
+            }
+            if (coversNight(start, end, night.minusDays(1))) {
+                nightsWithSleep.add(night.minusDays(1));
             }
         }
 
-        if (nightsWithSleep.isEmpty()) {
-            return new SleepAnalysisResult<>(DESCRIPTION, 0);
-        }
-
-        LocalDate firstNight = nightsWithSleep.stream().min(LocalDate::compareTo).get();
-        LocalDate lastNight = nightsWithSleep.stream().max(LocalDate::compareTo).get();
-
-        int totalNights = (int) (lastNight.toEpochDay() - firstNight.toEpochDay() + 1);
-        int sleeplessNights = totalNights - nightsWithSleep.size();
-
+        int sleeplessNights = allNights.size() - nightsWithSleep.size();
         return new SleepAnalysisResult<>(DESCRIPTION, sleeplessNights);
     }
 
